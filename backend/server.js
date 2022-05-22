b507b75da20b08c9b62ee4a0679e616a66496b4e
ahmed-razic <ahmed.razic@gmail.com>
2022-05-22 10:13:30 +0200

    server started

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotnev').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}.`)
+})
