d6cc2ae1011f6f43152e02fafe2811a926d2a297
ahmed-razic <ahmed.razic@gmail.com>
2022-05-27 17:05:42 +0200

    added frontend pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
